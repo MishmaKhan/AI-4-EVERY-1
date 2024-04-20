a875bad69ca1b3853929de9219ce9d7b8fc11b92
MishmaKhan <167611744+MishmaKhan@users.noreply.github.com>
2024-04-20 14:18:14 +0500

    Delete pipeline_text_example.py

--- pipeline_text_example.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-# classifier = pipeline(task = "text-classification")
-# classifier = pipeline(task = "text-classification",model = "distilbert/distilbert-base-uncased-finetuned-sst-2-english")
-model_path = ("../../Models/models--distilbert--distilbert-base-uncased-finetuned-sst-2-english/snapshots/714eb0fa89d2f80546fda750413ed43d93601a13")
-
-classifier = pipeline(task = "text-classification", model = model_path)
-
-print(classifier(["you are the best", "Get Lost"]))
